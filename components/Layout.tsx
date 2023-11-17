4c6cc87e2936f65259d7229789825c7d79bac0f2
Aaryan Kalra <aryan.smart832@gmail.com>
2023-11-18 00:15:07 +0530

    as

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,7 +7,7 @@ function Layout({ children }: { children: ReactNode }) {
     <div>
       <div className="grid grid-cols-4 h-screen mx-auto max-w-6xl ">
         <Sidebar />
-        <div className="col-span-3 md:col-span-2">{children}</div>
+        <div className="col-span-3 md:col-span-2 max-md:p-1">{children}</div>
         <FollowBar />
       </div>
     </div>
