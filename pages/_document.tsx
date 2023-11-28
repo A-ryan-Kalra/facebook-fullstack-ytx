082c4fa3e3aa8734ef9c5da6769c2189c0bd2a70
Aaryan Kalra <aryan.smart832@gmail.com>
2023-11-28 14:37:33 +0530

    new

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>MyHub</title>
+      </Head>
+
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
