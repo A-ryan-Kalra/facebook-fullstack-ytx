14853b38ad920cd280537cb5cf5cf60e35fc292e
Aaryan Kalra <aryan.smart832@gmail.com>
2024-01-03 20:44:17 +0530

    little changes on nextauthfile and notifications

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -43,14 +43,18 @@ export const authOptions: AuthOptions = {
       },
     }),
   ],
+  pages: {
+    signIn: "/",
+  },
   adapter: PrismaAdapter(prisma),
   debug: process.env.NODE_ENV === "development",
   session: {
     strategy: "jwt",
   },
-  jwt: {
-    secret: process.env.NEXTAUTH_JWT_SECRET,
-  },
+  // jwt: {
+  //   secret: process.env.NEXTAUTH_JWT_SECRET,
+  // },
+
   secret: process.env.NEXTAUTH_SECRET,
 };
 
