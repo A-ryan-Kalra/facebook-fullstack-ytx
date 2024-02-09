e972fa79f3605873f353b91157f91e1ac5f3d897
Aaryan Kalra <aryan.smart832@gmail.com>
2024-02-09 14:42:23 +0530

    changes on user's post

--- components/users/PostFeed.tsx
+++ components/users/PostFeed.tsx
@@ -1,7 +1,8 @@
 import usePosts from "@/hooks/usePosts";
 import useUser from "@/hooks/useUser";
-import React from "react";
+import React, { useEffect } from "react";
 import PostItem from "../posts/PostItem";
+import useCurrentUser from "@/hooks/useCurrentUser";
 
 interface PostFeedProps {
   userId?: string;
@@ -9,10 +10,16 @@ interface PostFeedProps {
 
 function PostFeed({ userId }: PostFeedProps) {
   const { data: posts, mutate } = usePosts(userId as string);
-  const { mutate: FetchedPostsMutate } = usePosts();
+  const { data: post, mutate: FetchedPostsMutate } = usePosts();
+  const { data: session } = useCurrentUser();
+
   //   console.log(posts);
-  mutate();
-  FetchedPostsMutate();
+  useEffect(() => {
+    if (session) {
+      mutate();
+      FetchedPostsMutate();
+    }
+  }, [posts, post, mutate, FetchedPostsMutate]);
   return (
     <div className="flex flex-col gap-2 mt-2">
       {posts?.map((post: Record<string, any>, index: number) => (
--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -2,17 +2,26 @@ import Form from "@/components/Form";
 import PostFeed from "@/components/users/PostFeed";
 import UserBio from "@/components/users/UserBio";
 import UserHero from "@/components/users/UserHero";
+import useCurrentUser from "@/hooks/useCurrentUser";
 import useUser from "@/hooks/useUser";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect } from "react";
 import { ClipLoader } from "react-spinners";
 
 function UserProfile() {
   const router = useRouter();
   const { userId } = router.query;
   const { data, error, isLoading, mutate } = useUser(userId as string);
+  const { data: session } = useCurrentUser();
+
+  useEffect(() => {
+    if (session) {
+      mutate();
+    }
+  }, [mutate, data]);
+
   if (isLoading || !data) {
     return (
       <div className="flex h-full justify-center items-center">
@@ -20,7 +29,7 @@ function UserProfile() {
       </div>
     );
   }
-  mutate();
+
   // console.log(isLoading);
   return (
     <div>
